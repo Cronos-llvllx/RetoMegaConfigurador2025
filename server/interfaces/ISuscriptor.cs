5f3d60657e7b20e866ae9b5eab7e6fa1876a761e
Aldo Kalid Hernandez Camarena <aldokalid.hc@outlook.com>
2025-06-28 13:12:30 -0600

    Agregado GetAlll en interfaz ISuscriptor para evitar repetición de código

--- server/interfaces/ISuscriptor.cs
+++ server/interfaces/ISuscriptor.cs
@@ -3,4 +3,11 @@ using megaapi.models;
 namespace megaapi.interfaces;
 
 /// <summary>Interfaz para Suscriptor.</summary>
-public interface ISuscriptor : IEntidad<Suscriptor, int> { }
+public interface ISuscriptor : IEntidad<Suscriptor, int>
+{
+  
+  /// <summary>
+  /// Otiene a todos los suscriptores, incluyendo colonia y ciudad.
+  /// </summary>
+  Task<List<Suscriptor>> GetAll();
+}
