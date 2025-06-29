9c5c4656c79f784e04b2f86b860fb2480e599fdc
Aldo Kalid Hernandez Camarena <aldokalid.hc@outlook.com>
2025-06-29 16:07:44 -0600

    Solucionado Promocion.cs no aceptando valores nulos

--- server/models/Promocion.cs
+++ server/models/Promocion.cs
@@ -3,7 +3,7 @@ namespace megaapi.models;
 public class Promocion()
 {
   public int Idpromocion { set; get; }
-  public byte Alcance { set; get; }
+  public byte? Alcance { set; get; }
   public string Nombre { set; get; } = null!;
   public int? Duracion { set; get; }
   public DateTime FechaRegistro { set; get; }
