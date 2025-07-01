0eb69f993716d7555990f72a6b9c6d92bc5dfd49
Marlene Aguilar Valle <marlene.aguilar.valle@gmail.com>
2025-06-30 18:53:13 -0600

    Creacion de componente de servicios

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -10,5 +10,10 @@ export const routes: Routes = [
     path: 'promociones',
     loadComponent: () =>
       import('./promociones/promociones.component').then(m => m.PromocionesComponent)
+  },
+  {
+    path: 'paquetes',
+    loadComponent: () =>
+      import('./paquetes/paquetes.component').then(m => m.PaquetesComponent)
   }
 ];
