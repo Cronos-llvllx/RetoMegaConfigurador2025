c50a95583bb4d40cb107ad6007a745b96167a25e
Aldo Kalid Hernandez Camarena <aldokalid.hc@outlook.com>
2025-06-30 22:18:46 -0600

    Trabajados endpoinst de backend para obtener promociones

--- server/controllers/CPromocion.cs
+++ server/controllers/CPromocion.cs
@@ -12,4 +12,29 @@ public class Promocion(IPromocion repo) : ControllerBase
   private readonly IPromocion _repo = repo;
 
   // ** Definir endpoints...
+  [HttpGet("")]
+  public async Task<IActionResult> ObtenerPromociones()
+  {
+    return Ok(await _repo.ObtenerTodoAsync());
+  }
+
+  [HttpGet("{id}")]
+  public async Task<IActionResult> ObtenerPromocionPorId(int id)
+  {
+    try
+    {
+      var promocion = await _repo.ObtenerPorIdAsync(id)
+        ?? throw new NullReferenceException();
+
+      return Ok(promocion);
+    }
+    catch (NullReferenceException)
+    {
+      return NotFound($"No se encontró la promoción con el id {id}");
+    }
+    catch (Exception ex)
+    {
+      return StatusCode(500, ex);
+    }
+  }
 }
--- server/repositories/RepoPromocion.cs
+++ server/repositories/RepoPromocion.cs
@@ -6,20 +6,30 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace megaapi.repositories;
+
 public class RepoPromocion(MEGADbContext dbContext) : IPromocion
 {
-    private readonly MEGADbContext _dbContext = dbContext;
-    public async Task<IEnumerable<Promocion>> ObtenerTodoAsync()
-    {
-        // CORREGIDO: Se usa 'Promociones' en plural
-        return await _dbContext.Promociones.ToListAsync();
-    }
-    public async Task<Promocion?> ObtenerPorIdAsync(int id)
-    {
-        // CORREGIDO: Se usa 'Promociones' en plural
-        return await _dbContext.Promociones.FindAsync(id);
-    }
-    public Task<Promocion> CrearAsync(Promocion promocion) => throw new NotImplementedException();
-    public Task<bool> ActualizarAsync(Promocion promocion) => throw new NotImplementedException();
-    public Task<bool> EliminarAsync(Promocion promocion) => throw new NotImplementedException();
+  private readonly MEGADbContext _dbContext = dbContext;
+  public async Task<IEnumerable<Promocion>> ObtenerTodoAsync()
+  {
+    // CORREGIDO: Se usa 'Promociones' en plural
+    return await _dbContext.Promociones
+      .Include(p => p.Ciudades)
+      .Include(p => p.Colonias)
+      .Include(p => p.Paquetes)
+      .ToListAsync();
+  }
+  public async Task<Promocion?> ObtenerPorIdAsync(int id)
+  {
+    // CORREGIDO: Se usa 'Promociones' en plural
+    return await _dbContext.Promociones
+      .Include(p => p.Ciudades)
+      .Include(p => p.Colonias)
+      .Include(p => p.Paquetes)
+      .SingleOrDefaultAsync(p => p.Idpromocion == id);
+
+  }
+  public Task<Promocion> CrearAsync(Promocion promocion) => throw new NotImplementedException();
+  public Task<bool> ActualizarAsync(Promocion promocion) => throw new NotImplementedException();
+  public Task<bool> EliminarAsync(Promocion promocion) => throw new NotImplementedException();
 }
