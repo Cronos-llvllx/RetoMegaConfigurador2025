a1ccbb53a2d91fe53ce10b23946c440c5afc83b1
Aldo Kalid Hernandez Camarena <aldokalid.hc@outlook.com>
2025-07-01 01:07:11 -0600

    Modificados interfaz de Paquete y Repositorio de Paquete para admitir CRUD

--- server/repositories/RepoPaquete.cs
+++ server/repositories/RepoPaquete.cs
@@ -9,10 +9,44 @@ namespace megaapi.repositories;
 
 public class RepoPaquete(MEGADbContext dbContext) : IPaquete
 {
-    private readonly MEGADbContext _dbContext = dbContext;
-    public async Task<IEnumerable<Paquete>> ObtenerTodoAsync()
-    {
-        // CORREGIDO: Se usa 'Paquetes' en plural
-        return await _dbContext.Paquetes.ToListAsync();
-    }
+  private readonly MEGADbContext _dbContext = dbContext;
+
+  public async Task<Paquete> ActualizarAsync(Paquete paquete)
+  {
+    _dbContext.Paquetes.Update(paquete);
+    await _dbContext.SaveChangesAsync();
+
+    return paquete;
+  }
+
+  public async Task<bool> CrearAsync(Paquete paquete)
+  {
+    _dbContext.Paquetes.Add(paquete);
+    await _dbContext.SaveChangesAsync();
+
+    return true;
+  }
+
+  public async Task<bool> EliminarAsync(Paquete paquete)
+  {
+    _dbContext.Paquetes.Remove(paquete);
+    await _dbContext.SaveChangesAsync();
+
+    return true;
+  }
+
+  public async Task<Paquete?> ObtenerPorIdAsync(int id)
+  {
+    return await _dbContext.Paquetes
+      .Include(p => p.Servicios)
+      .SingleOrDefaultAsync(p => p.Idpaquete == id);
+  }
+
+  public async Task<IEnumerable<Paquete>> ObtenerTodoAsync()
+  {
+    // CORREGIDO: Se usa 'Paquetes' en plural
+    return await _dbContext.Paquetes
+      .Include(p => p.Servicios)
+      .ToListAsync();
+  }
 }
